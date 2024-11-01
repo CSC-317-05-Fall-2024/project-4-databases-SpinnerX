dcea8c68d9bb97dc866ab1f1e2d76d152857aa69
type <type>
2024-11-01 12:15:36 -0700

    Fixed up submitting restaurants

--- server/public/scripts/restaurantSubmit.js
+++ server/public/scripts/restaurantSubmit.js
@@ -1,14 +1,17 @@
 // Function to be called when the button is clicked
 
+
+
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     // Target the button by its ID
-   
+    console.log("Reloaded DOM");
     // Add a click event listener to the button
    
     const form = document.getElementById('restaurantForm');
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', async function(event){
         event.preventDefault();
+        console.log("Submit Clicked!");
 
         // console.log("Submit Clicked!");
 
@@ -17,33 +20,36 @@ document.addEventListener('DOMContentLoaded', () => {
         const address = document.getElementById('address').value;
         const photoUrl = document.getElementById('photoUrl').value || 'https://picsum.photos/200/300'; // Fallback image
 
-        fetch('/api/new_restaurants', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name, address, phone, photoUrl
-            }),
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            // console.log(data);
-            window.location.href = `/new_restaurants/{data.id}`;
-        }).catch(error => {
-            console.error('error:', error);
-        });
+        try {
+            const response = await fetch('api/restaurants', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({name, address, phone, photoUrl}),
+            });
+
+            if (response.ok) {
+                console.log("Restaurant created successfully!");
+
+                window.location.href = '/new_restaurant';
+            } else {
+                console.error("Failed to create restaurant.");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
 
        this.reset();
 
     });
 
 
-    const container = document.querySelector('.cardContainer');
-    const spans = document.querySelectorAll('span');
+    // const container = document.querySelector('.cardContainer');
+    // const spans = document.querySelectorAll('span');
     
-        for(let span of spans){
-            console.log(span.id);
-        }
+    //     for(let span of spans){
+    //         console.log(span.id);
+    //     }
 
 });
